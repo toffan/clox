f078c1af5e7c405f09bce46b242bb596303f44c6
toffan <git.srv@toffan.org>
2023-06-22 13:01:20 +0200

    chall: support more than 256 constants

--- src/debug.c
+++ src/debug.c
@@ -25,6 +25,18 @@ static int constantInstruction(const char* name, Chunk* chunk, int offset) {
     return offset + 2;
 }
 
+static int constantLongInstruction(const char* name, Chunk* chunk, int offset) {
+    int constant = 0;
+    // Cannot read 32 bytes at once because data is not necessarily aligned
+    constant += chunk->code[offset + 1];
+    constant += chunk->code[offset + 2] << 8;
+    constant += chunk->code[offset + 3] << 16;
+    printf("%-16s %4d '", name, constant);
+    printValue(chunk->constants.values[constant]);
+    printf("'\n");
+    return offset + 4;
+}
+
 int disassembleInstruction(Chunk* chunk, int offset) {
     printf("%04d ", offset);
     if (offset > 0 && getLine(chunk, offset) == getLine(chunk, offset - 1)) {
@@ -37,6 +49,8 @@ int disassembleInstruction(Chunk* chunk, int offset) {
     switch (instruction) {
     case OP_CONSTANT:
         return constantInstruction("OP_CONSTANT", chunk, offset);
+    case OP_CONSTANT_LONG:
+        return constantLongInstruction("OP_CONSTANT_LONG", chunk, offset);
     case OP_RETURN:
         return simpleInstruction("OP_RETURN", offset);
     default:
--- src/main.c
+++ src/main.c
@@ -6,10 +6,9 @@ int main(int argc, char** argv) {
     Chunk chunk;
     initChunk(&chunk);
 
-    int constant = addConstant(&chunk, 1.2);
-    writeChunk(&chunk, OP_CONSTANT, 123);
-    writeChunk(&chunk, constant, 123);
-
+    for (int i = 0; i < 300; ++i) {
+        writeConstant(&chunk, i * 4, 123);
+    }
     writeChunk(&chunk, OP_RETURN, 123);
     disassembleChunk(&chunk, "test chunk");
     freeChunk(&chunk);
