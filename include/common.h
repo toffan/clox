fc229a26901a70c4a253f9e9f1311567c69c0b6b
toffan <git.srv@toffan.org>
2023-06-28 17:46:37 +0200

    ch17: Compiling Expressions

--- include/common.h
+++ include/common.h
@@ -5,6 +5,7 @@
 #include <stddef.h>
 #include <stdint.h>
 
+#define DEBUG_PRINT_CODE
 #define DEBUG_TRACE_EXECUTION
 
 #endif  // HG_62ed4388_c4b9_4703_98c6_94e4162aa120
--- include/compiler.h
+++ include/compiler.h
@@ -1,6 +1,8 @@
 #ifndef HG_06fc2a35_e2f6_4481_a312_c299d866dcbb
 #define HG_06fc2a35_e2f6_4481_a312_c299d866dcbb
 
-void compile(const char* source);
+#include "vm.h"
+
+bool compile(const char* source, Chunk* chunk);
 
 #endif  // HG_06fc2a35_e2f6_4481_a312_c299d866dcbb
